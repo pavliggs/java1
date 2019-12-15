b4168e76c6cde1fdfeae10e4ec645e0b14894b6f
pavliggs <51763058+pavliggs@users.noreply.github.com>
2019-12-15 20:05:33 +0300

    add homework H4 attempt 2

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -3,9 +3,9 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 public class TriangleSimpleInfo {
     public static int maxSide(int a, int b, int c) {
         //при помощи логического И составляем условия для определения наибольшей длины
-        if (a > b && a > c)
+        if (a >= b && a >= c)
             return a;
-        else if (b > a && b > c)
+        else if (b >= a && b >= c)
             return b;
         else
             return c;
@@ -13,9 +13,9 @@ public class TriangleSimpleInfo {
 
     public static int minSide(int a, int b, int c) {
         //при помощи логического И составляем условия для определения наименьшей длины
-        if (a < b && a < c)
+        if (a <= b && a <= c)
             return a;
-        else if (b < a && b < c)
+        else if (b <= a && b <= c)
             return b;
         else
             return c;
@@ -30,8 +30,8 @@ public class TriangleSimpleInfo {
     }
 
     public static void main(String[] args) {
-        System.out.println(maxSide(9, 18, 15));
-        System.out.println(minSide(5, 4, 7));
+        System.out.println(maxSide(8, 5, 10));
+        System.out.println(minSide(8, 10, 10));
         System.out.println(isEquilateralTriangle(9, 9, 9));
     }
 }
