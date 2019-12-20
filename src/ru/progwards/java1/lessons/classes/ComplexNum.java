fc6d4f480e6a3f5621ef2180c63f5d78b563a2fc
pavliggs <51763058+pavliggs@users.noreply.github.com>
2019-12-20 23:18:45 +0300

    add homework H5 attempt 3

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -1,35 +1,41 @@
 package ru.progwards.java1.lessons.classes;
 
 public class ComplexNum {
-    int a, b;
+    int a, b, a1, b1;
 
     public ComplexNum(int a, int b) {
         this.a = a;
         this.b = b;
     }
 
+    /*
+    параметром метода будет новый созданный объект со своими параметрами, далее по формуле производятся
+    действия параметров первого объекта с параметрами второго объекта, результат которых присваивается новым
+    переменным, которые будут являтся параметрами нового объекта, который возвращается в методе
+    (возвращается ссылка на объект)
+    */
     public ComplexNum add(ComplexNum num) {
-        a += num.a;
-        b += num.b;
-        return new ComplexNum(a, b);
+        a1 = a + num.a;
+        b1 = b + num.b;
+        return new ComplexNum(a1, b1);
     }
 
     public ComplexNum sub(ComplexNum num) {
-        a -= num.a;
-        b -= num.b;
-        return new ComplexNum(a, b);
+        a1 = a - num.a;
+        b1 = a - num.b;
+        return new ComplexNum(a1, b1);
     }
 
     public ComplexNum mul(ComplexNum num) {
-        a = a * num.a - b * num.b;
-        b = b * num.a + a * num.b;
-        return new ComplexNum(a, b);
+        a1 = a * num.a - b * num.b;
+        b1 = b * num.a + a * num.b;
+        return new ComplexNum(a1, b1);
     }
 
     public ComplexNum div(ComplexNum num) {
-        a = (a * num.a + b * num.b) / (num.a * num.a + num.b * num.b);
-        b = (b * num.a - a * num.b) / (num.a * num.a + num.b * num.b);
-        return new ComplexNum(a, b);
+        a1 = (a * num.a + b * num.b) / (num.a * num.a + num.b * num.b);
+        b1 = (b * num.a - a * num.b) / (num.a * num.a + num.b * num.b);
+        return new ComplexNum(a1, b1);
     }
 
     @Override
@@ -38,7 +44,7 @@ public class ComplexNum {
     }
 
     public static void main(String[] args) {
-        ComplexNum complexNum = new ComplexNum(1000, 1000);
+        ComplexNum complexNum = new ComplexNum(1, 1);
         System.out.println(complexNum.add(new ComplexNum(99, 99)));
         System.out.println(complexNum.sub(new ComplexNum(1, 1)));
         System.out.println(complexNum.mul(new ComplexNum(99, 99)));
