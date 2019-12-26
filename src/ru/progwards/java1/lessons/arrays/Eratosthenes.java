bf1c361b0633b9aebfc6982ce2cd1bcb3cbfc527
pavliggs <51763058+pavliggs@users.noreply.github.com>
2019-12-26 15:00:33 +0300

    add homework H6, attempt 7

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -12,16 +12,34 @@ public class Eratosthenes {
     }
 
     private void sift() {
-        for (int i = 2; i < this.sieve.length; i++) {
-            for (int j = 2 * i; j < this.sieve.length; j += i) {
-
+        /*
+        чтобы не повторялись уже просеянные числа - числа можно зачеркивать начиная сразу с числа i^2,
+        потому что все составные числа меньше него уже будут зачеркнуты к этому времени
+        (реализация займет немного меньше памяти). Метод подсмотрел в гугле :)
+        for (int i = 2; i * i < sieve.length; i++) - внешний цикл
+        for (int j = i * i; j < sieve.length; j += i) - внутренний цикл
+        */
+        for (int i = 2; i < sieve.length; i++) {
+            if (sieve[i] == true) {
+                for (int j = 2 * i; j < sieve.length; j += i) {
+                    sieve[j] = false;
+                }
             }
         }
     }
 
+    /*
+    передаем число, по которому хотим узнать простое оно или сложное
+    и возвращаем элемент массива с индексом равным этому числу
+    */
+    public boolean isSimple(int n) {
+        return sieve[n];
+    }
+
     public static void main(String[] args) {
-        Eratosthenes era = new Eratosthenes(10);
+        Eratosthenes era = new Eratosthenes(20);
         System.out.println(Arrays.toString(era.sieve));
+        System.out.println(era.isSimple(18));
     }
 
 }
