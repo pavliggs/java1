d9f50168def20d3ba3ec9e11d350d081a0e6254c
pavliggs <51763058+pavliggs@users.noreply.github.com>
2020-01-05 23:13:52 +0300

    add homework H7, attempt 2

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -6,18 +6,30 @@ public class SumBits {
         Необходимо определить когда нужно закончить цикл по сдвигу числа вправо.
         Для этого будет удобно воспользоваться переводом числа в строку и количество итераций цикла
         будет меньше длины строки.
-        Например: 011001 -> "11001" -> length = 5
+        Например: 011001 -> "011001" -> length = 5
         */
-        int valueLength = Integer.toBinaryString(value).length();
-        int res = 0;
-        for (int i = 0; i < valueLength; i++) {
-            res += value & 0b1;
-            value >>= 1;
-        }
-        return res;
+            /*
+            при помощи метода String.format получаем двоичное представление числа в 8-битовом формате (8 символов - %8s),
+            а при помощи метода replace заполняем пустые символы нулями
+            PS: хотя в принципе не обязательно, так как нули вначале числа при подсчёте суммы нам не нужны
+            */
+            int valueLength = String.format("%8s", Integer.toBinaryString(value)).replace(' ', '0').length();
+            int res = 0;
+            for (int i = 0; i < valueLength; i++) {
+                res += value & 0b1;
+                value >>= 1;
+            }
+            if (value >= 0)
+                return res;
+            else
+                /*
+                отрицательное число выводится в формате int (32 бита) и спереди числа стоят 24 единицы, а затем
+                само отрицательное число, поэтому вычитаем сумму этих единиц, которые нам не нужны при подсчёте
+                */
+                return res - 24;
     }
 
     public static void main(String[] args) {
-        System.out.println(sumBits((byte)0b1110));
+        System.out.println(sumBits((byte)0b10000000));
     }
 }
