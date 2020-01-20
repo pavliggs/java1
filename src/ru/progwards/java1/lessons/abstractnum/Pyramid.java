873f9adf59daceb8e37bf49646378f99581f4cfe
pavliggs <51763058+pavliggs@users.noreply.github.com>
2020-01-20 15:59:03 +0300

    intensive 2, attempt 4

--- src/ru/progwards/java1/lessons/abstractnum/Pyramid.java
+++ src/ru/progwards/java1/lessons/abstractnum/Pyramid.java
@@ -8,9 +8,14 @@ public class Pyramid extends Figure3D {
 
     @Override
     public Number volume() {
+        /*
+        * чтобы метод правильно работал для экземпляра класса IntNumber и экземпляра класса DoubleNumber
+        * необходимо задать условие и при определенном типе параметра класса Pyramid программа будем выполняться
+        * нужным способом
+        * */
         if (segment.getClass() == IntNumber.class)
             return segment.mul(segment.mul(segment).div(new IntNumber(3)));
         else
-            return segment.mul(segment.mul(segment).div(new DoubleNumber(3)));
+            return segment.mul(segment.mul(segment)).div(new DoubleNumber(3));
     }
 }
