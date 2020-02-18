d972490d4163b919cc16ea25aebf48106355cd4b
pavliggs <51763058+pavliggs@users.noreply.github.com>
2020-02-18 22:10:31 +0300

    homework 12, attempt 5

--- src/ru/progwards/java1/lessons/collections/MatrixIterator.java
+++ src/ru/progwards/java1/lessons/collections/MatrixIterator.java
@@ -18,31 +18,31 @@ public class MatrixIterator<T> implements Iterator<T> {
 
     @Override
     public T next() {
+        /* после того как мы возвратим последнний элемент в последней строке i увеличится на 1, что не совсем корректно
+         * если не использовать метод hasNext(), то программа вызовет исключение */
         if (i == array.length)
             throw new NoSuchElementException("No such element.");
         if (j == array[i].length - 1) {
             // сохраняем текущее значение j
             int saveValueJ = j;
-            // обнуляем
+            // обнуляем j, т.к. следующий элемент должен быть с нулевым индексом
             j = 0;
+            /* возвращаем последний элемент в строке за счет переменной saveValueJ, а после return
+            * i увеличится на 1, что гарантирует нам переход на следующую строку */
             return array[i++][saveValueJ];
         } else
-            return array[i][j++];
+            /* в самый первый вызов метода будет возвращен элемент array[0][0], а за счёт того, что я использую
+            * постфиксный инкремент, то j будет увеличиваться на 1 после return */
+            return array[i][j++]; //
     }
 
     public static void main(String[] args) {
         Integer[][] arr = {{1, 3, 9}, {4, 5, 13}, {10, 7, 6}};
-//        Integer[][] arr = {{1, 3, 9, 100}};
         MatrixIterator<Integer> matrixArr = new MatrixIterator<>(arr);
 
         while (matrixArr.hasNext()) {
             Integer intObj = matrixArr.next();
             System.out.print(intObj + " ");
         }
-//        System.out.println(matrixArr.next());
-//        System.out.println(matrixArr.next());
-//        System.out.println(matrixArr.next());
-//        System.out.println(matrixArr.next());
-//        System.out.println(matrixArr.next());
     }
 }
