ff2c101b8b8f7ca7d0f5643f4235c3339d73ce22
pavliggs <51763058+pavliggs@users.noreply.github.com>
2020-03-24 17:57:49 +0300

    homework 17, attempt 2

--- src/ru/progwards/java1/lessons/files/FilesSelect.java
+++ src/ru/progwards/java1/lessons/files/FilesSelect.java
@@ -7,29 +7,38 @@ import java.util.List;
 
 public class FilesSelect {
     public void selectFiles(String inFolder, String outFolder, List<String> keys) {
+        // задаём фильтр для файлов, которые будем искать (ищем только файлы с расширением txt)
         PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:**/*.txt");
 
         try {
             Files.walkFileTree(Paths.get(inFolder), new SimpleFileVisitor<>() {
                 @Override
                 public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
-                    if (pathMatcher.matches(path)) {
-                        String content;
-                        try {
-                            content = Files.readString(path);
+                    try {
+                        if (pathMatcher.matches(path)) {
+                            // записываем содержимое файла в переменную content
+                            String content = Files.readString(path);
                             for (int i = 0; i < keys.size(); i++) {
+                                // если content содержит подстроку равную элементу списка
                                 if (content.contains(keys.get(i))) {
+                                    // создаем новый путь, добавляя к outFolder путь с именем, равным элементу списка
                                     Path newFolder = Paths.get(outFolder).resolve(keys.get(i));
+                                    // если такой папки нет, то создаём её
                                     if (Files.notExists(newFolder))
+                                        // создаём папку на основании пути newFolder
                                         Files.createDirectory(newFolder);
+                                    /* создаем новый путь, добавляя к newFolder путь с именем, равным имени файла,
+                                     * путь которого передаётся в метод visitFile */
                                     Path newFile = newFolder.resolve(path.getFileName());
+                                    // создаём файл на основании пути newFile
                                     Files.createFile(newFile);
+                                    // записываем в новый файл содержимое переменной content
                                     Files.writeString(newFile, content);
                                 }
                             }
-                        } catch (IOException e) {
-                            System.out.println(e.getMessage());
                         }
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
                     }
                     return FileVisitResult.CONTINUE;
                 }
