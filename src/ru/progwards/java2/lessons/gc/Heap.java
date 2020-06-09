a729c61e2b652f339b3a2bd89ddf6e0c8e379818
pavliggs <51763058+pavliggs@users.noreply.github.com>
2020-06-09 16:12:33 +0300

    java2 homework 6, change 1

--- src/ru/progwards/java2/lessons/gc/Heap.java
+++ src/ru/progwards/java2/lessons/gc/Heap.java
@@ -5,62 +5,92 @@ import java.util.*;
 public class Heap {
     private byte[] bytes;
 
-    private List<byte[]> freeBlocks;
-    private List<byte[]> busyBlocks;
-    private Comparator<byte[]> comparator;
+    private List<Block> freeBlocks;
+    private List<Block> busyBlocks;
+
+    private int quantityCompact;
 
     Heap(int maxHeapSize) {
-        comparator = new Comparator<byte[]>() {
-            @Override
-            public int compare(byte[] o1, byte[] o2) {
-                return Integer.compare(o1[0], o2[0]);
-            }
-        };
         freeBlocks = new ArrayList<>();
         busyBlocks = new ArrayList<>();
         bytes = new byte[maxHeapSize];
-//        for (int i = 0; i < maxHeapSize; i++) {
-//            bytes[i] = i;
-//        }
-        freeBlocks.add(bytes);
+        freeBlocks.add(new Block(0, bytes.length));
+        quantityCompact = 0;
+    }
+
+    public static class Block implements Comparable<Block> {
+        private int startIndex;
+        private int size;
+
+        Block(int startIndex, int size) {
+            this.startIndex = startIndex;
+            this.size = size;
+        }
+
+        public int getStartIndex() {
+            return startIndex;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public void setStartIndex(int startIndex) {
+            this.startIndex = startIndex;
+        }
+
+        @Override
+        public String toString() {
+            return "Block{" +
+                    "startIndex=" + startIndex +
+                    ", size=" + size +
+                    '}';
+        }
+
+        @Override
+        public int compareTo(Block o) {
+            return Integer.compare(startIndex, o.startIndex);
+        }
     }
 
     public int malloc(int size) {
-        byte startIndex = 0;
+        int startIndex = 0;
         for (int i = 0; i < freeBlocks.size(); i++) {
-            byte[] freeBlock = freeBlocks.get(i);
-            // если блок нужного размера найден
-            if (freeBlock.length >= size) {
-                startIndex = freeBlock[0];
-                // создаём занятый блок
-                byte[] busyBlock = new byte[size];
-                int num = 0;
-                // заполняем блок значениями
-                for (byte j = startIndex; j < startIndex + size; j++) {
-                    busyBlock[num] = j;
-                    ++num;
-                }
-                busyBlocks.add(busyBlock);
-                busyBlocks.sort(comparator);
-                // перезаписываем свободный блок в список freeBlocks с новыми значениями
-                byte[] freeBlockNew = new byte[freeBlock.length - busyBlock.length];
-                System.arraycopy(freeBlock, size, freeBlockNew, 0, freeBlock.length - size);
-                freeBlocks.set(i, freeBlockNew);
+            Block freeBlock = freeBlocks.get(i);
+            // если подходящий по размеру блок существует, то помечаем его ячейки как занятые
+            if (size <= freeBlock.getSize()) {
+                startIndex = freeBlock.getStartIndex();
+                // добавляем блок в список занятых блоков
+                busyBlocks.add(new Block(startIndex, size));
+                // сортируем по значению свойства startIndex (объекты Block реализуют интерфейс Comparable)
+                busyBlocks.sort(null);
+                // создаём новый свободный блок с учётом уже занятых ячеек памяти
+                Block newFreeBlock = new Block(freeBlock.getStartIndex() + size, freeBlock.getSize() - size);
+                freeBlocks.set(i, newFreeBlock);
                 break;
             }
-            // свободный блок нужного размера не нашёлся
-            if (i == freeBlocks.size() - 1)
-                throw new OutOfMemoryException();
+            /*
+            * если свободных блоков нужного размера не нашлось, то вызываем метод compact() и снова и ищем подходящий
+            * свободный блок. Если во второй раз не нашлось свободных блоков, то значит свободной памяти не хватает
+            * и бросаем OutOfMemoryException
+            * */
+            if (i == freeBlocks.size() - 1) {
+                if (quantityCompact == 1)
+                    throw new OutOfMemoryException();
+                compact();
+                ++quantityCompact;
+                i = -1;
+            }
         }
         return startIndex;
     }
 
     public void free(int ptr) {
         for (int i = 0; i < busyBlocks.size(); i++) {
-            byte[] busyBlock = busyBlocks.get(i);
-            if (busyBlock[0] == ptr) {
+            Block busyBlock = busyBlocks.get(i);
+            if (busyBlock.getStartIndex() == ptr) {
                 freeBlocks.add(busyBlock);
-                freeBlocks.sort(comparator);
+                freeBlocks.sort(null);
                 busyBlocks.remove(i);
                 break;
             }
@@ -75,22 +105,19 @@ public class Heap {
         int size = freeBlocks.size();
         for (int count = 0; count < size; count++) {
             if (i < freeBlocks.size() - 1) {
-                byte[] freeBlock = freeBlocks.get(i);
-                byte[] freeBlockNext = freeBlocks.get(i+1);
+                Block freeBlockCurrent = freeBlocks.get(i);
+                Block freeBlockNext = freeBlocks.get(i+1);
                 /*
                 * если конечный индекс массива freeBlock и начальный индекс массива freeBlockNext являются смежными,
                 * то создаём новым массив newFreeBlock из массивов freeBlock и freeBlockNext
                 * затем удаляем массивы freeBlock и freeBlockNext из списка freeBlocks и добавляем в список массив
                 * newFreeBlock и сортируем список
                 * */
-                if (freeBlock[freeBlock.length - 1] + 1 == freeBlockNext[0]) {
-                    byte[] newFreeBlock = new byte[freeBlock.length + freeBlockNext.length];
-                    System.arraycopy(freeBlock, 0, newFreeBlock, 0, freeBlock.length);
-                    System.arraycopy(freeBlockNext, 0, newFreeBlock, freeBlock.length, freeBlockNext.length);
-                    freeBlocks.remove(freeBlock);
+                if (freeBlockCurrent.getStartIndex() + freeBlockCurrent.getSize() == freeBlockNext.getStartIndex()) {
+                    Block newFreeBlock = new Block(freeBlockCurrent.getStartIndex(), freeBlockCurrent.getSize() +
+                                            freeBlockNext.getSize());
+                    freeBlocks.set(i, newFreeBlock);
                     freeBlocks.remove(freeBlockNext);
-                    freeBlocks.add(newFreeBlock);
-                    freeBlocks.sort(comparator);
                 } else
                     ++i;
             }
@@ -98,56 +125,71 @@ public class Heap {
     }
 
     public void compact() {
+        int startIndex = 0;
         for (int i = 0; i < busyBlocks.size(); i++) {
-            byte[] busyBlock = busyBlocks.get(i);
-            for (int j = 0; j < busyBlock.length; j++) {
-
-            }
+            Block busyBlock = busyBlocks.get(i);
+            busyBlock.setStartIndex(startIndex);
+            startIndex += busyBlock.getSize();
         }
-    }
-
-    public String printElements(List<byte[]> list) {
-        StringBuilder str = new StringBuilder();
-        for (int i = 0; i < list.size(); i++) {
-            if (i == 0 && i == list.size() - 1) {
-                str.append("{ " + Arrays.toString(list.get(i)) + " }");
-                break;
-            }
-            if (i == 0) {
-                str.append("{ " + Arrays.toString(list.get(i)) + ", ");
-                continue;
-            }
-            if (i == list.size() - 1) {
-                str.append(Arrays.toString(list.get(i)) + " }");
-                continue;
-            }
-            str.append(Arrays.toString(list.get(i)) + ", ");
+        for (int i = 0; i < freeBlocks.size(); i++) {
+            Block freeBlock = freeBlocks.get(i);
+            freeBlock.setStartIndex(startIndex);
+            startIndex += freeBlock.getSize();
         }
-        return str.toString();
+        defrag();
     }
 
     public static void main(String[] args) {
-//        Heap heap = new Heap(14);
-//        System.out.println(heap.printElements(heap.freeBlocks));
-//
-//        System.out.println(heap.malloc(5));
-//
-//        System.out.println(heap.printElements(heap.freeBlocks));
-//        System.out.println(heap.printElements(heap.busyBlocks));
-//
-//        System.out.println(heap.malloc(3));
-//
-//        System.out.println(heap.printElements(heap.freeBlocks));
-//        System.out.println(heap.printElements(heap.busyBlocks));
-//
-//        heap.free(0);
-//
-//        System.out.println(heap.printElements(heap.freeBlocks));
-//        System.out.println(heap.printElements(heap.busyBlocks));
-//
+        Heap heap = new Heap(50);
+
+        System.out.println(heap.freeBlocks);
+
+        heap.malloc(14);
+        heap.malloc(7);
+
+        System.out.println(heap.freeBlocks);
+        System.out.println(heap.busyBlocks);
+
+        heap.free(14);
+
+        System.out.println(heap.freeBlocks);
+        System.out.println(heap.busyBlocks);
+
 //        heap.defrag();
 //
-//        System.out.println(heap.printElements(heap.freeBlocks));
-//        System.out.println(heap.printElements(heap.busyBlocks));
+//        System.out.println(heap.freeBlocks);
+//        System.out.println(heap.busyBlocks);
+
+        heap.malloc(5);
+
+        System.out.println(heap.freeBlocks);
+        System.out.println(heap.busyBlocks);
+
+        heap.malloc(9);
+
+        System.out.println(heap.freeBlocks);
+        System.out.println(heap.busyBlocks);
+
+        heap.free(0);
+
+        System.out.println(heap.freeBlocks);
+        System.out.println(heap.busyBlocks);
+
+        heap.malloc(21);
+
+        System.out.println(heap.freeBlocks);
+        System.out.println(heap.busyBlocks);
+//
+        heap.malloc(16);
+
+        System.out.println(heap.freeBlocks);
+        System.out.println(heap.busyBlocks);
+
+//        heap.compact();
+//
+//        System.out.println(heap.freeBlocks);
+//        System.out.println(heap.busyBlocks);
+
+
     }
 }
